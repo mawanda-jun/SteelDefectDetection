7f5064061b1156fba04bbf9c8baf07ad9e7ccb3c
Giovanni Cavallin <37183651+mawanda-jun@users.noreply.github.com>
2019-08-22 13:03:34 +0200

    added competition losses

---
+++ competition_losses.py
@@ -0,0 +1,22 @@
+from tensorflow.python.keras.losses import binary_crossentropy
+import tensorflow.python.keras.backend as K
+
+
+def dice_coef(y_true, y_pred, smooth=1):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def dice_loss(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = y_true_f * y_pred_f
+    score = (2. * K.sum(intersection) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return 1. - score
+
+
+def bce_dice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
